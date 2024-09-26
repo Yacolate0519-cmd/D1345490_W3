b58d7737fa1a07930b074b2bdc1303e1760bf5cf
Yacolate-cmd <yacolate0519@gmail.com>
2024-09-26 10:58:17 +0800

    Test

--- Test5.py
+++ Test5.py
@@ -1,2 +1,6 @@
 value = int(input("輸入三位數字:"))
 
+key = value % 2
+judge = bool(value and key) 
+
+print()
--- Test7.py
+++ Test7.py
@@ -1,5 +1,6 @@
-value = input("輸入一個十進制數字:")
-two = int(value,2)
-print(two)
-      
+value = int(input("輸入一個十進制數字:"))
+print(bin(value)[2:])
+print(oct(value)[2:])
+print(hex(value)[2:])
+     
 

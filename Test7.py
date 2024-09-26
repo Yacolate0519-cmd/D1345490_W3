8c1201c9af785a8cc3902e03875729428bdefc61
Yacolate-cmd <yacolate0519@gmail.com>
2024-09-26 10:42:55 +0800

    Damn

---
+++ Test7.py
@@ -0,0 +1,5 @@
+value = input("輸入一個十進制數字:")
+two = int(value,2)
+print(two)
+      
+
---
+++ Test8.py
@@ -0,0 +1,6 @@
+value = int(input("請輸入一個五位數: "))
+value1 = list(str(value))
+for i in value1:
+    print(i)
+    
+    
